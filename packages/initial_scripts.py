af081e6c6043c7e2fc2f5c172bd8bd673307f80b
orbina <48756153+orbina@users.noreply.github.com>
2020-07-08 22:52:38 +0200

    Update initial_scripts.py

--- packages/initial_scripts.py
+++ packages/initial_scripts.py
@@ -1,14 +1,1 @@
-#!/usr/bin/env python3
-#
-#   This config file should only contain a comma separated list of scripts to run.
-#   Note any scripts in this list will be executed in the beginning of the installation
-#   This will allow scripts for adding repositories etc to be run in advance of e.g. apt
-#
-#   Shell scripts can be places anywhere, however it is a good idea to place them in the <PKI_ROOT>/packages/config.
-#
-cfg = [
-    '{}/add_user.sh',
-    '{}/add_custom_directories.sh',
-    #'{}/install_atom.sh',
-    '{}/msf_init.sh'
-]
+
