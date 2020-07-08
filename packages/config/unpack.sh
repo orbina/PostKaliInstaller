86da562b4f25aa2bbf64397f907567df246fbcf2
orbina <48756153+orbina@users.noreply.github.com>
2020-07-08 23:04:11 +0200

    Update unpack.sh

--- packages/config/unpack.sh
+++ packages/config/unpack.sh
@@ -4,4 +4,4 @@
 #
 
 /usr/bin/gunzip /usr/share/wordlists/rockyou.txt.gz
-/usr/bin/wget -O /home/${PKIUSR}/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg
+/usr/bin/wget -O /home/user/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg
