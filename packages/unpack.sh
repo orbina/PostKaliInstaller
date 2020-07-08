4b6178d6c3a8f04d2237cc836cbcd2561ca31675
orbina <48756153+orbina@users.noreply.github.com>
2020-07-08 22:54:32 +0200

    Delete unpack.sh

--- packages/unpack.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-#   This script unpacks the zipped files
-#
-
-/usr/bin/gunzip /usr/share/wordlists/rockyou.txt.gz
-/usr/bin/wget -O /home/${PKIUSR}/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg
