51e97f5d9f8dbc04242a3b087f22a718e3190234
orbina <48756153+orbina@users.noreply.github.com>
2020-07-09 10:58:11 +0200

    Update dependencies.sh

--- packages/config/dependencies.sh
+++ packages/config/dependencies.sh
@@ -4,8 +4,6 @@
 #
 
 /usr/bin/gem install winrm winrm-fs stringio
-/usr/bin/apt install python3
-/usr/bin/apt install python3-pip -y
 /usr/bin/python3 -m pip install colorama
 /usr/bin/python3 -m pip install toml
 /usr/bin/python3 -m pip install appdirs
