8d72fac752c8172da25c7cd18e79245b31263d0f
orbina <48756153+orbina@users.noreply.github.com>
2020-07-09 11:10:55 +0200

    Update unpack.sh

--- packages/config/unpack.sh
+++ packages/config/unpack.sh
@@ -3,5 +3,5 @@
 #   This script unpacks the zipped files
 #
 
-/usr/bin/gunzip /usr/share/wordlists/rockyou.txt.gz
-#/usr/bin/wget -O /home/user/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg
+/usr/bin/gunzip /usr/share/wordlists/rockyou.txt.gz 2>> ${PKILOG}
+#/usr/bin/wget -O /home/user/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg 2>> ${PKILOG}
