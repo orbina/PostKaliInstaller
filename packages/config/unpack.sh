c72438bd9c5bc80aeff18b9728194903ebd8a003
orbina <48756153+orbina@users.noreply.github.com>
2020-07-09 11:46:02 +0200

    Update unpack.sh

--- packages/config/unpack.sh
+++ packages/config/unpack.sh
@@ -4,4 +4,6 @@
 #
 
 /usr/bin/gunzip /usr/share/wordlists/rockyou.txt.gz 2>> ${PKILOG}
-#/usr/bin/wget -O /home/user/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg 2>> ${PKILOG}
+/usr/bin/tar xzf /home/${PKIUSR}/pki/git/dictionaries/seclists/Passwords/Leaked-Databases/rockyou.txt.tar.gz -C /home/${PKIUSR}/pki/git/dictionaries/seclists/Passwords/Leaked-Databases/ 2>> ${PKILOG}
+/usr/bin/tar xzf /home/${PKIUSR}/pki/git/dictionaries/seclists/Passwords/Leaked-Databases/rockyou-withcount.txt.tar.gz -C /home/${PKIUSR}/pki/git/dictionaries/seclists/Passwords/Leaked-Databases/ 2>> ${PKILOG}
+#/usr/bin/wget -O /home/${PKIUSR}/Pictures/wallpaper.jpg https://i.imgur.com/MSiSPq3.jpg 2>> ${PKILOG}
